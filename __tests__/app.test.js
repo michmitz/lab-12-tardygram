f8d4945aa0dfc8ea1a47a1bb21dc27164a7bd768
Michelle Stermitz <stermitzm@gmail.com>
2020-09-28 15:20:26 -0700

    added user model with signup route, user-service

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,23 @@ const app = require('../lib/app');
 
 describe('lab-12-tardygram routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+
+  it('signs up a user via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+        profilePhotoUrl: 'testurl.com' 
+      });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'test@test.com',
+      profilePhotoUrl: 'testurl.com' 
+    });
+  });
+
 });
---
+++ sql/setup.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL,
+  profile_photo_url TEXT NOT NULL
+);
